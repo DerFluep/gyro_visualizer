0537a4fc70bbc192fa4f17d250803d4b4f5ead2a
DerFluep <36232559+DerFluep@users.noreply.github.com>
2026-03-01 20:46:38 +0100

    Visualizer: Error handling & reading magic bytes

--- visualizer/src/main.rs
+++ visualizer/src/main.rs
@@ -1,28 +1,40 @@
 use std::time::Duration;
 
 fn main() {
-    let mut port = serialport::new("/dev/ttyACM0", 9600)
-        .timeout(Duration::from_millis(300))
+    let mut port = serialport::new("/dev/ttyACM0", 57600)
+        .timeout(Duration::from_millis(250))
         .open()
         .expect("Failed to open port");
-    port.set_baud_rate(57600).unwrap();
-    ::std::thread::sleep(Duration::from_millis(5000));
 
+    let mut serial_buf = [0u8; 1];
     let mut buf = [0u8; 8];
-    let mut start = [0u8; 1];
+    let header = [0xAA, 0xBB, 0xCC, 0xDD];
+    let mut sync_state = 0;
 
     loop {
-        'checking: loop {
-            port.read_exact(&mut start).unwrap();
-            if start[0] == 0 {
-                break 'checking;
+        match port.read_exact(&mut serial_buf) {
+            Ok(_) => {}
+            Err(e) => {
+                println!("Can't read serial: {}", e);
+                continue;
             }
         }
-        port.read_exact(&mut buf).unwrap();
-        let roll = f32::from_be_bytes(buf[0..4].try_into().unwrap());
-        let pitch = f32::from_be_bytes(buf[4..8].try_into().unwrap());
-        println!("Roll: {}", roll);
-        println!("Pitch: {}", pitch);
-        println!("______");
+        if serial_buf[0] == header[sync_state] {
+            sync_state += 1;
+            if sync_state == 4 {
+                match port.read_exact(&mut buf) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error reading floats: {}", e);
+                    }
+                }
+                let roll = f32::from_be_bytes(buf[0..4].try_into().unwrap());
+                let pitch = f32::from_be_bytes(buf[4..8].try_into().unwrap());
+                println!("Roll: {}", roll);
+                println!("Pitch: {}", pitch);
+                println!("______");
+                sync_state = 0;
+            }
+        }
     }
 }
