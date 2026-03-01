8f2e649a16ac67e6b25cd9a1a219607e8df76771
DerFluep <36232559+DerFluep@users.noreply.github.com>
2026-03-01 19:38:45 +0100

    Visualizer: print raw floats

--- visualizer/src/main.rs
+++ visualizer/src/main.rs
@@ -2,18 +2,14 @@ use std::time::Duration;
 
 fn main() {
     let mut port = serialport::new("/dev/ttyACM0", 9600)
-        .timeout(Duration::from_millis(250))
+        .timeout(Duration::from_millis(300))
         .open()
         .expect("Failed to open port");
-    port.set_baud_rate(9600).unwrap();
+    port.set_baud_rate(57600).unwrap();
     ::std::thread::sleep(Duration::from_millis(5000));
 
-    let mut buf = [0u8; 6];
+    let mut buf = [0u8; 8];
     let mut start = [0u8; 1];
-
-    let mut acc_x: i16;
-    let mut acc_y: i16;
-    let mut acc_z: i16;
 
     loop {
         'checking: loop {
@@ -23,12 +19,10 @@ fn main() {
             }
         }
         port.read_exact(&mut buf).unwrap();
-        acc_x = i16::from_be_bytes(buf[0..2].try_into().unwrap());
-        acc_y = i16::from_be_bytes(buf[2..4].try_into().unwrap());
-        acc_z = i16::from_be_bytes(buf[4..6].try_into().unwrap());
-        println!("{}", acc_x as f32 / 16384.0);
-        println!("{}", acc_y as f32 / 16384.0);
-        println!("{}", acc_z as f32 / 16384.0);
+        let roll = f32::from_be_bytes(buf[0..4].try_into().unwrap());
+        let pitch = f32::from_be_bytes(buf[4..8].try_into().unwrap());
+        println!("Roll: {}", roll);
+        println!("Pitch: {}", pitch);
         println!("______");
     }
 }
